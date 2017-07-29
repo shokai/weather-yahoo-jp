919abc2c34cc7abf7ff8f94bc862a57cf7c94300
Sho Hashimoto <hashimoto@shokai.org>
2017-07-29 15:44:04 +0700

    replacing Promise with async-await

--- packages/weather-yahoo-jp/src/yolp.js
+++ packages/weather-yahoo-jp/src/yolp.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
-import _ from 'lodash'
 import util from './util'
+import _ from 'lodash'
 
 const debug = require('debug')('weather-yahoo-jp:yolp')
 
@@ -13,69 +13,63 @@ export default class Yolp {
     })
   }
 
-  get (params) {
-    if (typeof params !== 'object') return Promise.reject('params required')
-    params = _.assign(params, this.defaultParams)
-    if (!params.coordinates) return Promise.reject('param "coordinates" is missing')
+  async get (params) {
+    if (typeof params !== 'object') throw new Error('params required')
+    params = Object.assign(params, this.defaultParams)
+    if (!params.coordinates) throw new Error('param "coordinates" is missing')
     if (!params.appid) params.appid = this.appid
-    const coordinates = _.values(params.coordinates).join(encodeURI(' '))
+    const coordinates = Object.values(params.coordinates).join(encodeURIComponent(' '))
     delete params.coordinates
-    return axios({
+    const res = await axios({
       method: 'get',
       url: `http://weather.olp.yahooapis.jp/v1/place?coordinates=${coordinates}`,
       params: params
-    }).then((res) => {
-      debug(res.data.ResultInfo)
-      return res.data
     })
+    debug(res.data.ResultInfo)
+    return res.data
   }
 
-  getWeather (params) {
-    if (typeof params !== 'object') return Promise.reject('params required')
+  async getWeather (params) {
+    if (typeof params !== 'object') throw new Error('params required')
     const mapParams = params.map
     delete params.map
     const coordinates = params.coordinates
-    return this.get(params)
-      .then((res) => {
-        let result = {}
-        for (let feature of res.Feature) {
-          let [lon, lat] = feature.Geometry.Coordinates.split(',').map(i => { return i - 0 })
-          let where = _
-                .chain(coordinates)
-                .toPairs()
-                .minBy((i) => {
-                  let [lon2, lat2] = i[1].split(',')
-                  return Math.abs(lon - lon2) + Math.abs(lat - lat2)
-                })
-                .first()
-                .value()
-          let weathers = feature.Property.WeatherList.Weather
-          let observation = _.find(weathers, (i) => { return i.Type === 'observation' })
-          result[where] = {
-            where: where,
-            geo: {
-              latitude: lat,
-              longitude: lon
-            },
-            map: this.getMap(_.assign({lat: lat, lon: lon, z: 10, overlay: 'type:rainfall'}, mapParams)),
-            observation: {
-              rain: observation.Rainfall,
-              date: util.parseDateString(observation.Date)},
-            forecast: weathers
-              .filter((i) => {
-                return i.Type === 'forecast'
-              }).map((i) => {
-                return {
-                  rain: i.Rainfall,
-                  date: util.parseDateString(i.Date)
-                }
-              }).sort((a, b) => {
-                return a.date > b.date
-              })
-          }
-        }
-        return result
-      })
+    const res = await this.get(params)
+
+    const result = {}
+    for (let feature of res.Feature) {
+      const [lon, lat] = feature.Geometry.Coordinates.split(',').map(i => { return i - 0 })
+      const where = _
+        .chain(coordinates)
+        .toPairs()
+        .minBy((i) => {
+          let [lon2, lat2] = i[1].split(',')
+          return Math.abs(lon - lon2) + Math.abs(lat - lat2)
+        })
+        .first()
+        .value()
+      const weathers = feature.Property.WeatherList.Weather
+      const observation = weathers.find(i => i.Type === 'observation')
+      result[where] = {
+        where: where,
+        geo: {
+          latitude: lat,
+          longitude: lon
+        },
+        map: this.getMap(Object.assign({lat: lat, lon: lon, z: 10, overlay: 'type:rainfall'}, mapParams)),
+        observation: {
+          rain: observation.Rainfall,
+          date: util.parseDateString(observation.Date)},
+        forecast: weathers
+          .filter(i => i.Type === 'forecast')
+          .map(i => ({
+            rain: i.Rainfall,
+            date: util.parseDateString(i.Date)
+          }))
+          .sort((a, b) => a.date > b.date)
+      }
+    }
+    return result
   }
 
   // http://developer.yahoo.co.jp/webapi/map/openlocalplatform/v1/static.html
