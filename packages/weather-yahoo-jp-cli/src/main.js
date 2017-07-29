70e04846097d3bcbd2d1c25d13ef1bb3e97443bd
Sho Hashimoto <hashimoto@shokai.org>
2017-07-29 16:06:58 +0700

    use async-await in CLI

--- packages/weather-yahoo-jp-cli/src/main.js
+++ packages/weather-yahoo-jp-cli/src/main.js
@@ -4,27 +4,22 @@ import pkg from '../package.json'
 import {temperatureString} from './util'
 import {forecast} from 'weather-yahoo-jp'
 
-module.exports.handler = function (argv) {
+module.exports.handler = async function (argv) {
   if (argv.length < 1) {
-    console.log(`${pkg.name} v${pkg.version}`)
-    console.log()
-    console.log(' % weather-yahoo-jp [地名...]')
+    console.log(
+`${pkg.name} v${pkg.version}
+
+ % weather-yahoo-jp [地名...]`)
     process.exit(1)
   }
 
-  argv.forEach((where) => {
-    forecast
-      .get(where)
-      .then((res) => {
-        const msgs = []
-        msgs.push(res.where)
-        msgs.push(`${res.today.date} ${res.today.text} ${temperatureString(res.today.temperature)}`)
-        msgs.push(`${res.tomorrow.date} ${res.tomorrow.text} ${temperatureString(res.tomorrow.temperature)}`)
-        msgs.push(res.url)
-        console.log(msgs.join('\n'))
-      })
-      .catch(function (err) {
-        console.error(err.stack || err)
-      })
-  })
+  for (let where of argv) {
+    const res = await forecast.get(where)
+    const msgs = []
+    msgs.push(res.where)
+    msgs.push(`${res.today.date} ${res.today.text} ${temperatureString(res.today.temperature)}`)
+    msgs.push(`${res.tomorrow.date} ${res.tomorrow.text} ${temperatureString(res.tomorrow.temperature)}`)
+    msgs.push(res.url)
+    console.log(msgs.join('\n'))
+  }
 }
